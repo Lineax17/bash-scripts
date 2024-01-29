1a1ee9b564615c6d29f14402668a2d80b2320a35
Lineax17 <144595200+Lineax17@users.noreply.github.com>
2024-01-29 11:32:28 +0100

    Update fedora_base_install.sh

--- fedora_base_install.sh
+++ fedora_base_install.sh
@@ -1,27 +1,11 @@
 #!bin/bash
 
-sudo dnf update -y
-
 sudo dnf upgrade -y
-
-sudo dnf install neofetch -y
 
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 
-sudo dnf install code -y
+sudo dnf install code neofetch nodejs git R openjdk-17-jdk -y
 
-sudo dnf install nodejs -y
-
-sudo dnf install git -y
-
-sudo dnf install R -y
-
-sudo dnf install openjdk-17-jdk
-
-sudo flatpak install Brave -y
-
-sudo flatpak install discordapp -y
-
-sudo flatpak install IntelliJ IDEA Ultimate -y
+sudo flatpak install Brave discordapp IntelliJ IDEA Ultimate -y
