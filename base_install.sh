31679ff88f9e8d7882acd08c737a47e9d18fa916
Lineax17 <144595200+Lineax17@users.noreply.github.com>
2024-01-29 12:22:58 +0100

    Update base_install.sh

--- base_install.sh
+++ base_install.sh
@@ -1,11 +1,29 @@
 #!bin/bash
 
+# Upgrade system
 sudo dnf upgrade -y
 
+# Adding VS Code repo
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 
-sudo dnf install code neofetch nodejs git R openjdk-17-jdk -y
+# Adding Brave repo
+sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
 
-sudo flatpak install Brave discordapp IntelliJ IDEA Ultimate -y
+sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
+
+# Installing apps via dnf
+sudo dnf install code brave neofetch nodejs git R openjdk-17-jdk -y
+
+# Check if flathub repo is already added
+if flatpak remote-list | grep -q 'flathub'
+then
+    echo "Flathub ist bereits hinzugefügt."
+else
+    echo "Flathub wird hinzugefügt..."
+    flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+fi
+
+# Installing apps via flathub
+sudo flatpak install flathub com.discordapp.Discord com.jetbrains.PyCharm-Professional com.jetbrains.IntelliJ-IDEA-Ultimate -y
