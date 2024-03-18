a22abc061fabcf60002f094e6461329750526566
Lineax17 <144595200+Lineax17@users.noreply.github.com>
2024-03-18 12:43:45 +0100

    Create base_install.sh

---
+++ apt/base_install.sh
@@ -0,0 +1,40 @@
+#!bin/bash
+
+# Updating system
+sudo apt update && apt upgrade -y
+
+# Installing apps with apt
+sudo apt install curl wget gpg flatpak -y
+
+# Adding Brave repo and installing it
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+
+sudo apt update && apt install brave-browser -y
+
+# Adding VS-Code repo and installing it
+
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+
+rm -f packages.microsoft.gpg
+
+sudo apt install apt-transport-https -y
+
+sudo apt update && apt install code -y
+
+# Check if flathub repo is already added
+if flatpak remote-list | grep -q 'flathub'
+then
+    echo "Flathub is already added."
+else
+    echo "Adding flathub repository..."
+    sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+fi
+
+# Installing apps via flathub
+sudo flatpak install flathub com.github.Eloston.UngoogledChromium -y
