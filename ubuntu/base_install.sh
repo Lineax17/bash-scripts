f40838db6d4d28eca6761aa7d3d4188c8dab1d45
Lineax17 <144595200+Lineax17@users.noreply.github.com>
2024-10-21 21:56:38 +0200

    Update base_install.sh

--- ubuntu/base_install.sh
+++ ubuntu/base_install.sh
@@ -22,10 +22,6 @@ sudo snap install discord
 sudo snap install spotify
 sudo snap install code --classic 
 sudo snap install node --classic 
-sudo snap install slack
-sudo snap install intellij-idea-ultimate --classic 
-sudo snap install pycharm-professional --classic
-sudo snap install webstorm --classic
 
 # Check if flathub repo is already added
 if flatpak remote-list | grep -q 'flathub'
