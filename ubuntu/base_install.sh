f733f7d7785a6c3ff6a2773112608668eaa44bcb
Lineax17 <144595200+Lineax17@users.noreply.github.com>
2024-12-11 10:02:54 +0100

    Update base_install.sh

--- ubuntu/base_install.sh
+++ ubuntu/base_install.sh
@@ -49,3 +49,4 @@ fi
 sudo flatpak install flathub com.github.Eloston.UngoogledChromium -y
 sudo flatpak install flathub tv.plex.PlexDesktop -y
 sudo flatpak install flathub com.vixalien.sticky -y
+sudo flatpak install org.filezillaproject.Filezilla -y
