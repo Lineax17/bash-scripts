7f9df480debb1c42d7dca3dfef9be52a666aebb0
Lineax17 <144595200+Lineax17@users.noreply.github.com>
2025-01-13 15:03:31 +0100

    Create flatpak_base_install.sh

---
+++ atomic/flatpak_base_install.sh
@@ -0,0 +1,20 @@
+#!bin/bash
+
+# Check if flathub repo is already added
+if flatpak remote-list | grep -q 'flathub'
+then
+    echo "Flathub is already added."
+else
+    echo "Adding flathub repository..."
+    sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+fi
+
+# Installing apps via flathub
+sudo flatpak install flathub tv.plex.PlexDesktop -y
+sudo flatpak install flathub com.vixalien.sticky -y
+sudo flatpak install org.filezillaproject.Filezilla -y
+sudo flatpak install it.mijorus.gearlever -y
+sudo flatpak install io.github.flattool.Warehouse -y
+sudo flatpak install org.mozilla.Thunderbird -y
+sudo flatpak install com.discordapp.Discord -y
+sudo flatpak install com.github.KRTirtho.Spotube -y
