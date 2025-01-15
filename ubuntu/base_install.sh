b9a081f0df3ef1e0f6896e84c2689d5ab506e538
Lineax17 <144595200+Lineax17@users.noreply.github.com>
2025-01-15 22:08:46 +0100

    Adding flatpaks manual

--- ubuntu/base_install.sh
+++ ubuntu/base_install.sh
@@ -36,35 +36,25 @@ sudo apt update
 
 sudo apt install zotero -y
 
-# Run Flatpak base script
-#!/bin/bash
-
-# Aktuelles Verzeichnis des Skripts ermitteln
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-# Zielskript relativ zum aktuellen Skriptverzeichnis
-SCRIPT="$SCRIPT_DIR/../atomic/flatpak_base_install.sh"
-
-# Debugging: Pfad überprüfen
-echo "Resolved script path: $SCRIPT"
-
-# Existenzprüfung
-if [ ! -f "$SCRIPT" ]; then
-    echo "Error: The script $SCRIPT does not exist."
-    exit 1
+# Check if flathub repo is already added
+if flatpak remote-list | grep -q 'flathub'
+then
+    echo "Flathub is already added."
+else
+    echo "Adding flathub repository..."
+    sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 fi
 
-# Prüfen, ob ausführbar, und falls nicht, Berechtigungen setzen
-if [ ! -x "$SCRIPT" ]; then
-    echo "The script $SCRIPT is not executable. Setting execute permissions..."
-    chmod +x "$SCRIPT" || { echo "Failed to set execute permissions."; exit 1; }
-fi
+# Installing apps via flathub
+sudo flatpak install flathub tv.plex.PlexDesktop -y
+sudo flatpak install flathub com.vixalien.sticky -y
+sudo flatpak install org.filezillaproject.Filezilla -y
+sudo flatpak install it.mijorus.gearlever -y
+sudo flatpak install io.github.flattool.Warehouse -y
+sudo flatpak install org.mozilla.Thunderbird -y
+sudo flatpak install com.discordapp.Discord -y
+sudo flatpak install com.github.KRTirtho.Spotube -y
 
-# Zielskript ausführen
-echo "Executing $SCRIPT..."
-"$SCRIPT" || { echo "The script $SCRIPT failed to execute properly."; exit 1; }
-
-echo "$SCRIPT has been executed successfully."
 
 
 
