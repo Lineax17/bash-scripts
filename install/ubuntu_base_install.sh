fd1eb133a254aa97de08128a3f3178cdfde2264f
Lineax17 <philipp.hartmann04@gmail.com>
2025-01-20 11:49:49 +0100

    replace flatpak install with flatpak script

--- install/ubuntu_base_install.sh
+++ install/ubuntu_base_install.sh
@@ -67,25 +67,15 @@ sudo apt update
 
 sudo apt install zotero -y
 
-# Check if flathub repo is already added
-if flatpak remote-list | grep -q 'flathub'
-then
-    echo "Flathub is already added."
-else
-    echo "Adding flathub repository..."
-    sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+# Run flatpak_base_install.sh
+SCRIPT="./flatpak_base_install.sh"
+
+if [ ! -x "$SCRIPT" ]; then
+    echo "The script $SCRIPT is not executable. Setting execute permissions..."
+    chmod +x "$SCRIPT"
 fi
 
-# Installing apps via flathub
-sudo flatpak install flathub tv.plex.PlexDesktop -y
-sudo flatpak install flathub com.vixalien.sticky -y
-sudo flatpak install org.filezillaproject.Filezilla -y
-sudo flatpak install it.mijorus.gearlever -y
-sudo flatpak install io.github.flattool.Warehouse -y
-sudo flatpak install org.mozilla.Thunderbird -y
-sudo flatpak install com.discordapp.Discord -y
-sudo flatpak install org.gimp.GIMP -y
-sudo flatpak install md.obsidian.Obsidian -y
-sudo flatpak install org.libreoffice.LibreOffice -y
+"$SCRIPT"
 
+echo "$SCRIPT has been executed."
 
