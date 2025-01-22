4db5d66967e265289c5de8c50ab1068e67c55a61
Lineax17 <philipp.hartmann04@gmail.com>
2025-01-22 15:09:22 +0100

    removing install that are no longer working after dnf 5 update

--- install/fedora_base_install.sh
+++ install/fedora_base_install.sh
@@ -15,19 +15,8 @@ echo "$SCRIPT has been executed."
 # Upgrade system
 sudo dnf update -y
 
-# Adding VS Code repo
-sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-
-sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
-
-# Sudo authentification not working, brave install not working
-# Adding Brave repo
-sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
-
-sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
-
 # Installing apps via dnf
-sudo dnf install code brave-browser neofetch nodejs R python3 python-is-python3 nextcloud-client ipython3 -y
+sudo dnf install nodejs R python3 python-is-python3 nextcloud-client ipython3 -y
 
 # Adding Steam repo and installing it
 sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
