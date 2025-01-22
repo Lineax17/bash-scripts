26c448469b39e129f187f61d3f5b9cb3ceea0793
Lineax17 <philipp.hartmann04@gmail.com>
2025-01-22 15:02:20 +0100

    marking errors with comments

--- install/fedora_base_install.sh
+++ install/fedora_base_install.sh
@@ -20,6 +20,7 @@ sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 
+# Sudo authentification not working, brave install not working
 # Adding Brave repo
 sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
 
