6929deab84e835a110f083547efab75674fa4d54
Lineax17 <philipp.hartmann04@gmail.com>
2025-01-24 20:35:59 +0100

    renaming to correct error

--- install/flatpak_base_install.sh
+++ install/flatpak_base_install.sh
@@ -13,13 +13,13 @@ fi
 # Installing apps via flathub
 sudo flatpak install flathub tv.plex.PlexDesktop -y
 sudo flatpak install flathub com.vixalien.sticky -y
-sudo flatpak install org.filezillaproject.Filezilla -y
-sudo flatpak install it.mijorus.gearlever -y
-sudo flatpak install io.github.flattool.Warehouse -y
-sudo flatpak install org.mozilla.Thunderbird -y
-sudo flatpak install com.discordapp.Discord -y
-sudo flatpak install org.gimp.GIMP -y
-sudo flatpak install md.obsidian.Obsidian -y
-sudo flatpak install org.libreoffice.LibreOffice -y
-sudo flatpak install com.makemkv.MakeMKV -y
-sudo flatpak install com.usebottles.bottles -y
+sudo flatpak install flathub org.filezillaproject.Filezilla -y
+sudo flatpak install flathub it.mijorus.gearlever -y
+sudo flatpak install flathub io.github.flattool.Warehouse -y
+sudo flatpak install flathub org.mozilla.Thunderbird -y
+sudo flatpak install flathub com.discordapp.Discord -y
+sudo flatpak install flathub org.gimp.GIMP -y
+sudo flatpak install flathub md.obsidian.Obsidian -y
+sudo flatpak install flathub org.libreoffice.LibreOffice -y
+sudo flatpak install flathub com.makemkv.MakeMKV -y
+sudo flatpak install flathub com.usebottles.bottles -y
