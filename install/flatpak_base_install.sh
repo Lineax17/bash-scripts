129f9000645bb6405da685075e13181c9abf26cd
Lineax17 <philipp.hartmann04@gmail.com>
2025-02-11 10:33:31 +0100

    add spot (spotify frontend) install

--- install/flatpak_base_install.sh
+++ install/flatpak_base_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Check if flathub repo is already added
+# Add flathub repo for easier use in terminal
 if flatpak remote-list | grep -q 'flathub'
 then
     echo "Flathub is already added."
@@ -9,20 +9,35 @@ else
     sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 fi
 
-# Modify installs to point directly to flathub
-# Installing apps via flathub
-flatpak install flathub tv.plex.PlexDesktop -y
-flatpak install flathub com.vixalien.sticky -y
-flatpak install flathub org.filezillaproject.Filezilla -y
-flatpak install flathub it.mijorus.gearlever -y
+# System utilities
+
+# App for management of installed flatpaks
 flatpak install flathub io.github.flattool.Warehouse -y
+
+# Graphical flatpak permission center
+flatpak install flathub com.github.tchx84.Flatseal -y
+
+# App for installing and updating Appimages
+flatpak install flathub it.mijorus.gearlever -y
+
+# Wine frontend for emulating windows apps
+flatpak install flathub com.usebottles.bottles -y
+
+# Minimal notes app
+flatpak install flathub com.vixalien.sticky -y
+
+# Office suite 
+flatpak install flathub org.libreoffice.LibreOffice -y
+flatpak install flathub org.onlyoffice.desktopeditors -y
+
+
+# Apps
+flatpak install flathub dev.alextren.Spot
+flatpak install flathub tv.plex.PlexDesktop -y
+flatpak install flathub org.filezillaproject.Filezilla -y
 flatpak install flathub org.mozilla.Thunderbird -y
 flatpak install flathub com.discordapp.Discord -y
 flatpak install flathub org.gimp.GIMP -y
 flatpak install flathub md.obsidian.Obsidian -y
-flatpak install flathub org.libreoffice.LibreOffice -y
 flatpak install flathub com.makemkv.MakeMKV -y
-flatpak install flathub com.usebottles.bottles -y
-flatpak install flathub org.onlyoffice.desktopeditors -y
 flatpak install flathub com.github.iwalton3.jellyfin-media-player -y
-flatpak install flathub com.github.tchx84.Flatseal -y
