dbd927d8ea107e914bb16f48d48ceb381533497f
Lineax17 <144595200+Lineax17@users.noreply.github.com>
2025-03-09 19:28:07 +0100

    adding description

--- update/fedora_update_system.sh
+++ update/fedora_update_system.sh
@@ -4,8 +4,10 @@ sudo dnf update -y --refresh
 
 flatpak update -y
 
-# Rebuild Nvidia drivers
+# Rebuild Nvidia drivers (Nvidia users only)
 
 sudo akmods
+
+# Rebuild inital ram filesystem (all users)
 
 sudo dracut -f --regenerate-all
