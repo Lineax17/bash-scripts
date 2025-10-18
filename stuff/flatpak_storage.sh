1c8381a6685ed97d5074f5fc65ecd58510601dfe
Lineax17 <philipp.hartmann04@gmail.com>
2025-10-18 15:34:08 +0200

    update to show individual app size

--- stuff/flatpak_storage.sh
+++ stuff/flatpak_storage.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 echo "System: $(du -sh /var/lib/flatpak 2>/dev/null | cut -f1), User: $(du -sh ~/.local/share/flatpak 2>/dev/null | cut -f1)"
+
+flatpak list --system --columns=installation,size
+flatpak list --user --columns=installation,size
