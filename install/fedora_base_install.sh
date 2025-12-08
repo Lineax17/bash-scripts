b7f73fb5ac882b7e85ec8eb79767349e801bc66e
Lineax17 <philipp.hartmann04@gmail.com>
2025-12-08 23:33:17 +0100

    add rpm build tools

--- install/fedora_base_install.sh
+++ install/fedora_base_install.sh
@@ -4,14 +4,18 @@
 sudo dnf update -y
 
 # Installing apps via dnf
-sudo dnf install fish nodejs R python3 python-is-python3 nextcloud-client ipython3 qemu virt-manager figlet -y
+sudo dnf install fish nodejs R python3 python-is-python3 nextcloud-client ipython3 qemu virt-manager -y
 
 # Installing build dependencies 
 sudo dnf install make gcc patch zlib-devel bzip2 bzip2-devel readline-devel sqlite sqlite-devel openssl-devel tk-devel libffi-devel xz-devel libuuid-devel gdbm-libs libnsl2 -y
 
+# Installing rpm-build-tools
+sudo dnf install rpm-build rpmdevtools rpmlint mock fedora-packager -y
+
+
 # Installing godot mono dependencies
 
-#sudo dnf install -y dotnet-sdk-8.0
+sudo dnf install -y dotnet-sdk-8.0
 
 # Install brave
 sudo dnf install dnf-plugins-core
@@ -30,10 +34,6 @@ sudo dnf install code -y
 # Install podman-docker compatibility
 
 sudo dnf install podman podman-compose podman-docker
-
-# Install docker from rpm repos
-#sudo dnf install docker-cli docker-compose
-#sudo usermod -aG docker $USER
 
 # Adding Steam repo and installing it
 sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
